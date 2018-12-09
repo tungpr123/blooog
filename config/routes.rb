af2213d4f53b8e81b20f477aef54482850e1f69f
Alim <tungpr123@mail.ru>
2018-12-09 23:40:37 +0600

    video7-8

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'posts#index', as: 'home'
 
   get 'about' => 'pages#about', as: 'about'
-  get 'contakti' => 'pages#contakti'
 
-  resources :posts
+
+  resources :posts do
+    resources :comments  
+  end
 end
