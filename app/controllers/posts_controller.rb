77129e0aac24e679a3ae70d472f19a02dc134457
Alim <tungpr123@mail.ru>
2018-12-09 23:41:39 +0600

    video8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   http_basic_authenticate_with name: "admin", password: "mstitel00",
     except: [:index, :show]
 
+
   def index
     @post = Post.all
   end
